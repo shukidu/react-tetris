028e3e33501747d39d545fd736486ab2eaf79bcd
Shuki Du <shukidu1217@gmail.com>
2024-04-04 19:32:16 -0700

    initial files

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import App from './containers/';
+import './unit/const';
+import './control';
+import { subscribeRecord } from './unit';
+
+subscribeRecord(store); // store updated status into localStorage
+
+render(
+    <Provider store={store}>
+         <App />
+     </Provider>,
+     document.getElementById('root')
+);
