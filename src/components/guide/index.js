94fbf7730ffda74a37c1131b774434283119d598
Shuki Du <shukidu1217@gmail.com>
2024-05-04 14:32:02 -0700

    guide index js file

---
+++ src/components/guide/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import QRCode from 'qrcode';
+import style from './index.less';
+import { transform, i18n, lan } from '../../unit/const';
+import { isMobile } from '../../unit';
+
